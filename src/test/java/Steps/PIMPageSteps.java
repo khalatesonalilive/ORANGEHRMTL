a9a2b944475caf906bd4be87dbc2a65465a7f45c
khalatesonalilive <khalatesonali1234@gmail.com>
2024-01-16 14:51:28 +0530

    PIMPage Test Cases are created Successfully.

---
+++ src/test/java/Steps/PIMPageSteps.java
@@ -0,0 +1,31 @@
+package Steps;
+
+import org.testng.Assert;
+
+import BaseLayer.BaseClass;
+import PageLayer.PIMPage;
+import io.cucumber.java.en.Given;
+
+public class PIMPageSteps extends BaseClass {
+	public static PIMPage pIMPage;
+
+	@Given("user is on PIM Page and validate PIM Page URL")
+	public void user_is_on_pim_page_and_validate_pim_page_url() throws InterruptedException {
+
+		pIMPage = new PIMPage();
+		pIMPage.clickPimLink();
+
+		String actualUrl = pIMPage.getPIMPageUrl();
+		boolean ac = actualUrl.contains("pim");
+		Assert.assertEquals(ac, true);
+
+	}
+
+	@Given("user click on Add Employee button and enter firstname , lastname and click on save button")
+	public void user_click_on_add_employee_button_and_enter_firstname_lastname_and_click_on_save_button() throws InterruptedException {
+		pIMPage.clickAddEmployeeButton();
+		pIMPage.addFnameLname("Shivay", "Mahadevan");
+		
+	}
+}
+
