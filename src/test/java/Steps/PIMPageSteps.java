45c14e6f3c8196c9d417fa71de6615d6a159ca98
khalatesonalilive <khalatesonali1234@gmail.com>
2024-01-16 15:46:35 +0530

    EmployeeID is captured successfully.

--- src/test/java/Steps/PIMPageSteps.java
+++ src/test/java/Steps/PIMPageSteps.java
@@ -5,9 +5,11 @@ import org.testng.Assert;
 import BaseLayer.BaseClass;
 import PageLayer.PIMPage;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 
 public class PIMPageSteps extends BaseClass {
 	public static PIMPage pIMPage;
+	public static String empid;
 
 	@Given("user is on PIM Page and validate PIM Page URL")
 	public void user_is_on_pim_page_and_validate_pim_page_url() throws InterruptedException {
@@ -22,10 +24,21 @@ public class PIMPageSteps extends BaseClass {
 	}
 
 	@Given("user click on Add Employee button and enter firstname , lastname and click on save button")
-	public void user_click_on_add_employee_button_and_enter_firstname_lastname_and_click_on_save_button() throws InterruptedException {
+	public void user_click_on_add_employee_button_and_enter_firstname_lastname_and_click_on_save_button()
+			throws InterruptedException {
 		pIMPage.clickAddEmployeeButton();
 		pIMPage.addFnameLname("Shivay", "Mahadevan");
-		
-	}
-}
 
+	}
+
+	@When("user capture Employee Id")
+	public void user_capture_employee_id() throws InterruptedException {
+		empid = pIMPage.captureEmployeeID();
+	}
+
+	@When("user click on Employee List button")
+	public void user_click_on_employee_list_button() throws InterruptedException {
+		pIMPage.clickEmployeeList();
+	}
+
+}
